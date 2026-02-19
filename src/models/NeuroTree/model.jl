e704a3839bd28ee14f5639b355a79b724e962ae6
AdityaPandeyCN <adityapand3y666@gmail.com>
2026-02-19 14:27:07 +0530

    revert comments
    Signed-off-by: AdityaPandeyCN <adityapand3y666@gmail.com>

--- src/models/NeuroTree/model.jl
+++ src/models/NeuroTree/model.jl
@@ -22,12 +22,13 @@ function NeuroTree((feats, outs)::Pair{<:Integer,<:Integer}; tree_type=:binary, 
     return NeuroTree(tree_type, actA, scaler, feats, outs, depth, trees, nodes, leaves, Float32(init_scale))
 end
 
+# Define the Lux interface
 function LuxCore.initialparameters(rng::AbstractRNG, l::NeuroTree)
     return (
-        w=Float32.((rand(rng, l.nodes * l.trees, l.feats) .- 0.5) ./ 4),
-        b=zeros(Float32, l.nodes * l.trees),
-        s=Float32.(fill(log(exp(1) - 1), l.nodes * l.trees)),
-        p=Float32.(randn(rng, l.outs, l.leaves * l.trees) .* l.init_scale),
+        w=Float32.((rand(rng, l.nodes * l.trees, l.feats) .- 0.5) ./ 4), # w
+        b=zeros(Float32, l.nodes * l.trees), # b
+        s=Float32.(fill(log(exp(1) - 1), l.nodes * l.trees)), # s
+        p=Float32.(randn(rng, l.outs, l.leaves * l.trees) .* l.init_scale), # p
     )
 end
 
@@ -40,18 +41,21 @@ end
 
 function (l::NeuroTree)(x, ps, st)
     if l.scaler
-        nw = softplus(ps.s) .* (l.actA(ps.w) * x .+ ps.b)
+        nw = softplus(ps.s) .* (l.actA(ps.w) * x .+ ps.b) # [F,B] => [NT,B]
     else
-        nw = (l.actA(ps.w) * x .+ ps.b)
+        nw = (l.actA(ps.w) * x .+ ps.b) # [F,B] => [NT,B]
     end
-    nw = reshape(nw, size(st.ml, 2), :)
-    lw = exp.(st.ml * nw .- st.ms * softplus.(nw))
-    lw = reshape(lw, :, size(x, 2))
-    y = ps.p * lw ./ l.trees
+    nw = reshape(nw, size(st.ml, 2), :) # [NT,B] => [N,TB]
+    lw = exp.(st.ml * nw .- st.ms * softplus.(nw)) # [N,TB] => [L,TB]
+    lw = reshape(lw, :, size(x, 2)) # [L,TB] => [LT,B]
+    y = ps.p * lw ./ l.trees # [P,LT] * [LT,B] => [P,B]
     return y, st
 end
 
 
+"""
+    get_logits_mask(::Val{:binary}, depth::Integer)
+"""
 function get_logits_mask(::Val{:binary}, depth::Integer)
     nodes = 2^depth - 1
     leaves = 2^depth
@@ -80,6 +84,9 @@ function get_logits_mask(::Val{:oblivious}, depth::Integer)
     return mask
 end
 
+"""
+    get_softplus_mask(::Val{:binary}, depth::Integer)
+"""
 function get_softplus_mask(::Val{:binary}, depth::Integer)
     nodes = 2^depth - 1
     leaves = 2^depth
